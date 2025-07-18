58eec9f434d5e224ea61dba554d872c372050ec3
tarun-2024 <tk550744@gmail.com>
2025-07-18 19:02:44 +0530

    firstChanges

---
+++ src/Components/inputField.jsx
@@ -0,0 +1,15 @@
+const InputField = ({type, placeholder, icon}) => {
+    return (
+        <div className="input-wrapper">
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="input-field"
+                required
+            />
+            <i className="material-symbols-rounded">{icon}</i>
+        </div>
+    );
+};
+
+export default InputField;
